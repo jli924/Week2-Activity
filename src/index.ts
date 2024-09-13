d542d06886c2a34c9e84bb0b2a38063bb03f56fd
holdenc20 <kcholden@me.com>
2024-09-13 14:47:17 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
+const result_divide = calc.divide(6, 2);
+console.log(`result_divide = ${result_divide}`)
+
