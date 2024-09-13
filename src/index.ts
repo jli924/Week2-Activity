e4cbd384c3cd013302d10e47515ce7c2e2a14d0d
holdenc20 <kcholden@me.com>
2024-09-13 14:56:07 -0400

    adding merge changes

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ console.log(`result_add = ${result_add}`);
 const result_subtract = calc.subtract(2, 3);
 console.log(`result_subt = ${result_subtract}`)
 
+const result_divide = calc.divide(6, 2);
+console.log(`result_divide = ${result_divide}`)
+
